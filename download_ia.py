866e9d65bb965ff1f010e452ad2887fa77a54763
Will Howes <willmhowes@gmail.com>
2023-12-11 15:44:47 -0800

    put ia/whois logic into functions

--- download_ia.py
+++ download_ia.py
@@ -2,26 +2,43 @@ import os
 import requests
 import yaml
 import json
+import datetime
+import streamlit as st
 
-with open("urls.yml", "r") as stream:
-    try:
-        urls = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
 
-responses = []
-
-for earl in urls:
-    if os.path.isfile(f"iacdx/{earl}.json"):
-        continue
-
-    ia_request = f"https://web.archive.org/cdx/search/cdx?url={earl}&fl=timestamp&output=json&limit=1"
-
-    print("requesting", earl)
-
+@st.cache_data
+def download_iacdx(url):
+    ia_request = f"https://web.archive.org/cdx/search/cdx?url={url}&fl=timestamp&output=json&limit=1"
+    print("requesting", url)
     ia_response = requests.get(ia_request).json()
+    return ia_response
 
-    responses.append(ia_response)
 
-    with open(f"iacdx/{earl}.json", "w", encoding="utf-8") as f:
-        json.dump(ia_response, f, indent=2)
+def download_iacdx_bulk(urls):
+    responses = []
+    for i, url in enumerate(urls):
+        ia_response = download_iacdx(url)
+        ia_response["count"] = i + 1
+        responses.append(ia_response)
+    return responses
+
+
+def download_iacdx_earliestdate(url):
+    ia_response = download_iacdx(url)
+    if len(ia_response) > 0:
+        try:
+            ia_earliest_date = ia_response[1][0]
+            ia_earliest_date = (
+                ia_earliest_date[0:4]
+                + "-"
+                + ia_earliest_date[4:6]
+                + "-"
+                + ia_earliest_date[6:8]
+            )
+            ia_earliest_date = datetime.date.fromisoformat(ia_earliest_date)
+            ia_response = ia_earliest_date
+        except ValueError:
+            print("Error formatting creation date, returning raw value")
+        return ia_response
+    else:
+        return "Unavailable"
--- download_whois.py
+++ download_whois.py
@@ -2,35 +2,38 @@ import os
 import requests
 import yaml
 import json
+import datetime
 from dotenv import load_dotenv
-
-load_dotenv()
-
-# def download_whois():
+import streamlit as st
 
 
-with open("urls.yml", "r") as stream:
-    try:
-        urls = yaml.safe_load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-responses = []
-
-for i, earl in enumerate(urls):
-    if os.path.isfile(f"whois/{earl}.json"):
-        continue
-
-    whois_request = f"https://whoisjsonapi.com/v1/{earl}"
-
-    print("requesting", earl)
-
+@st.cache_data
+def download_whois(url):
+    load_dotenv()
+    whois_request = f"https://whoisjsonapi.com/v1/{url}"
+    print("requesting", url)
     whois_response = requests.get(
         whois_request, headers={"Authorization": "Bearer " + os.getenv("API_KEY")}
     ).json()
-    whois_response["count"] = i + 1
+    return whois_response
 
-    responses.append(whois_response)
 
-    with open(f"whois/{earl}.json", "w", encoding="utf-8") as f:
-        json.dump(whois_response, f, indent=2)
+def download_whois_bulk(urls):
+    responses = []
+    for i, url in enumerate(urls):
+        whois_response = download_whois(url)
+        whois_response["count"] = i + 1
+        responses.append(whois_response)
+    return responses
+
+
+def download_whois_creationdate(url):
+    whois_response = download_whois(url)
+    whois_creation_date = whois_response["domain"]["created_date"]
+    try:
+        candidate_date = datetime.datetime.fromisoformat(whois_creation_date)
+        candidate_date = candidate_date.date()
+        whois_creation_date = candidate_date
+    except ValueError:
+        print("Error formatting creation date, returning raw value")
+    return whois_creation_date
